3fc74f867e635bfc74bde2458ec48b18961fb0ca
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-06 22:54:49 +0400

    feat: added while loop

---
+++ JS/While Loop.js
@@ -0,0 +1,14 @@
+// Pick a target number we are trying to guess
+const target = Math.floor(Math.random() * 10);
+// Make initial guess
+let guess = Math.floor(Math.random() * 10);
+
+// Continue looping while guess is not the target num
+while (guess !== target) {
+	console.log(`Target: ${target} Guess: ${guess}`);
+	// IMPORTANT!!
+	// Update the value of guess each time through the loop
+	guess = Math.floor(Math.random() * 10);
+}
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log(`CONGRATS YOU WIN!!`);
