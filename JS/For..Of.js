1bd9c482afea26320b32e45aeca00d56158f4282
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-07 13:16:55 +0400

    feat: added for..of loop

---
+++ JS/For..Of.js
@@ -0,0 +1,13 @@
+let subreddits = [ 'soccer', 'popheads', 'cringe', 'books' ];
+// With a standard for loop
+for (let i = 0; i < subreddits.length; i++) {
+	console.log(subreddits[i]);
+}
+//Much cleaner  with a for...of loop!
+for (let sub of subreddits) {
+	console.log(sub);
+}
+//Works with other iterables, like strings!
+for (let char of 'cockadoodledoo') {
+	console.log(char.toUpperCase());
+}
