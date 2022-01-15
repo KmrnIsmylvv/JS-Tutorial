51cdcd661864bcbd546b82549e6adfadddb467d6
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-15 19:03:38 +0400

    refactor: Update object-destructing

--- JS/Object-Destructing.js
+++ JS/Object-Destructing.js
@@ -5,11 +5,11 @@ const runner = {
     title: "Elder of the Order of the Golden Heart of Kenya"
   }
   
-  // const {
-  //   first,
-  //   last,
-  //   time
-  // } = runner;
+  const {
+    first,
+    last,
+    time
+  } = runner;
   
   const {
     country: nation,
